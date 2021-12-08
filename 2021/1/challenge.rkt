adb8f93edea81ba055c5cefa136fd610c06e5399
rej696 <55083730+rej696@users.noreply.github.com>
2021-12-08 22:30:30 +0000

    update racket implementation of challenge 1

--- 2021/1/challenge.rkt
+++ 2021/1/challenge.rkt
@@ -4,17 +4,24 @@
 
 (define (inc value) (+ value 1))
 
-(define (read in value prev)
-  (let ([line (read-line in)])
-    (if (eof-object? line)
-       (display value)
-       (let ([cur (string->number line)])
-         (read in
-              (if (> cur prev)
-                 (inc value)
-                 value)
-              cur)))))
+
+; Multiple arity function using match
+(define (read . arg)
+  (match arg
+    [(list in value prev)
+     (let ([line (read-line in)])
+       (if (eof-object? line)
+          (display value)
+          (let ([cur (string->number line)])
+            (read in
+                 (if (> cur prev)
+                    (inc value)
+                    value)
+                 cur))))]
+    [(list in)
+     (read in -1 0)]))
 
 
-(call-with-input-file "1/input.txt"
-  (lambda (in) (read in -1 0)))
+
+(call-with-input-file "2021/1/input.txt"
+  read)
