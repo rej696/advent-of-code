39d092a371eebfe14a4b49d5d4bf714741858a3d
rej696 <55083730+rej696@users.noreply.github.com>
2022-11-30 18:17:49 +0000

    complete challenge in rust

--- 2022/aoc1/src/main.rs
+++ 2022/aoc1/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let mut elves: Vec<i32> = include_str!("input")
+        .trim()
+        .split("\n\n")
+        .map(|elf_lines| {
+            elf_lines
+                .trim()
+                .lines()
+                .map(|line| line.parse::<i32>().unwrap())
+                .fold(0, |acc, x| acc + x)
+        })
+        .collect();
+
+    elves.sort_by(|a, b| b.cmp(a));
+    println!("{}", elves[0]);
+    println!("{}", elves[0] + elves[1] + elves[2]);
 }
